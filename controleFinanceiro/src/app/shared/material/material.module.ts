699ffb10a4203f075cc014a06f92e564164daae7
genivalc <genivalcandeiadon.neto@gmail.com>
2023-07-13 13:56:12 -0300

    config modules

--- controleFinanceiro/src/app/shared/material/material.module.ts
+++ controleFinanceiro/src/app/shared/material/material.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatCardModule } from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatSelectModule
+  ],
+  exports: [
+    MatCardModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
