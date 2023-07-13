82c08741343f55b84bd241e5cb5e0d716297fc21
genivalc <genivalcandeiadon.neto@gmail.com>
2023-07-13 16:17:14 -0300

    creat inversao de dependecia

---
+++ controleFinanceiro/src/app/shared/base/http-base.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpBaseService {
+  //apenas leitura
+  public readonly httpClient!: HttpClient
+  private apiBase = 'http://localhost:3000/';
+
+  constructor(protected readonly injector: Injector) {
+    //inversao de dependecia , cria um bloco de codigo para deixar a ultilizacao dinamica sem a necesidade de ser especifico o tipo (exemplo bloco de codigo dinamico para ultizar qualquer banco de dados)
+    if(!injector) {
+      throw new Error ('Not injector null')
+
+    }
+    this.httpClient = injector.get(HttpClient)
+
+  }
+
+  public httpGet(endpoint: String): Observable<any> {
+    return this.httpClient.get(`${this.apiBase}${endpoint}`)
+  }
+
+  public httpPost(endpoint: String, dados: any): Observable<any> {
+    return this.httpClient.post(`${this.apiBase}${endpoint}`, dados)
+  }
+
+  public httpPut(endpoint: String, dados: any): Observable<any> {
+    return this.httpClient.put(`${this.apiBase}${endpoint}`, dados)
+  }
+
+  public httpDelete(endpoint: String): Observable<any> {
+    return this.httpClient.delete(`${this.apiBase}${endpoint}`)
+  }
+}
